bc9744627b739c0693dbbd24434b3d694cf4b2c8
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-08-11 14:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
 function decimalToBinary(x) {
   //Write you code here
-	let num = 0;
+	let result = 0;
 	 while(true){
-        num = (num * 10 )+ x % 2
+        result = (result * 10 )+ x % 2
         x = Math.floor(x/2);
         if(x==0){
-            return num;
+            return result;
         }
     }   
 }
