4b651beeb7ba46952dbbb96a7d75e6032eee854f
kanekii16 <96989943+kanekii16@users.noreply.github.com>
2023-08-11 14:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 
-function decimalToBinary(num) {
+function decimalToBinary(x) {
   //Write you code here
-	    console.log(num%2);
-        num = Math.floor(num/2);
-        
+	let num = 0;
+	 while(true){
+        num = (num * 10 )+ x % 2
+        x = Math.floor(x/2);
+        if(x==0){
+            return num;
+        }
+    }   
 }
 
 window.decimalToBinary = decimalToBinary;
